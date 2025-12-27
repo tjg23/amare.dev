e0d5aaaf9721ff020a016631cfbe31d5673a32ed
Tyler Gorton <tjgorton23@icloud.com>
2025-12-27 13:15:34 -0600

    Begin rewriting 'About' info

--- src/site.config.ts
+++ src/site.config.ts
@@ -4,7 +4,7 @@ const siteConfig: SiteConfig = {
 	website: 'https://example.com',
 	title: 'Tyler Gorton',
 	subtitle: 'Full Stack Developer',
-	description: 'Personal portfolio and blog of Tyler Gorton, a full stack developer specializing in modern web technologies and thoughtful design.',
+	description: 'Personal portfolio and blog of Tyler Gorton, a senior in Software Engineering and Philosophy at Iowa State University.',
 	image: {
 		src: '/dante-preview.jpg',
 		alt: 'Dante - Astro.js and Tailwind CSS theme'
@@ -47,20 +47,6 @@ const siteConfig: SiteConfig = {
 			href: 'https://twitter.com/'
 		}
 	],
-	hero: {
-		title: "Hello, I'm Tyler",
-		text:
-			"I'm currently a senior in Software Engineering and Philosophy at Iowa State University. " +
-			"I'm passionate about building web applications that are not only functional but also provide a great user experience. " +
-			"Feel free to explore my projects and blog posts to learn more about my work and interests."
-		,
-		actions: [
-			{
-				text: 'Get in Touch',
-				href: '/contact'
-			}
-		]
-	},
 	subscribe: {
 		enabled: true,
 		title: 'Subscribe to my Newsletter',
